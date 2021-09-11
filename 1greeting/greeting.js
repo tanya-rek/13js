784053465903fda243804d7adf19e38aff26f02f
Tanya Rekun <rekun.tetiana@gmail.com>
2021-09-11 00:58:29 -0400

    Task with the greeting and task with the calculator are finished

---
+++ 1greeting/greeting.js
@@ -0,0 +1,5 @@
+function showMessage() {
+    let name = prompt ('What is your name?', '');
+
+    alert('Hello, ' + `${name}` + '!');
+}
---
+++ 2calculator/index.js
@@ -0,0 +1,27 @@
+function calcSum() {
+    let firstNumber = prompt ('Please enter the first number:', '');
+    let secondNumber = prompt ('Please enter the second number:', '');
+
+    alert(+firstNumber + +secondNumber);
+}
+
+function calcDeduction() {
+    let firstNumber = prompt ('Please enter the first number:', '');
+    let secondNumber = prompt ('Please enter the second number:', '');
+
+    alert(firstNumber - secondNumber);
+}
+
+function calcMultiplication() {
+    let firstNumber = prompt ('Please enter the first number:', '');
+    let secondNumber = prompt ('Please enter the second number:', '');
+
+    alert(firstNumber * secondNumber);
+}
+
+function calcDivision() {
+    let firstNumber = prompt ('Please enter the first number:', '');
+    let secondNumber = prompt ('Please enter the second number:', '');
+
+    alert(+firstNumber / +secondNumber);
+}
